7920da47ee48aed7eabc403fb8840a4e23d6f798
s21017 <s21017@std.it-college.ac.jp>
2022-12-01 17:17:09 +0900

    first commit

---
+++ app/src/main/java/jp/ac/it_college/std/s21017/myapplicationispoke/MainActivity.kt
@@ -0,0 +1,31 @@
+package jp.ac.it_college.std.s21017.myapplicationispoke
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.google.gson.Gson
+import jp.ac.it_college.std.s21020.pokemonquiz.databinding.ActivityMainBinding
+import java.io.InputStreamReader
+
+lateinit var pokemon: PokemonJson
+
+lateinit var pokedex: PokedexJson
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val inputStream =
+            assets?.open("ordered_pokemon.json")
+        val jsonReader = InputStreamReader(inputStream, "UTF-8").readText()
+        pokemon = Gson().fromJson(jsonReader, PokemonJson::class.java)
+
+        val inputStream2 =
+            assets?.open("filtered_pokedex.json")
+        val jsonReader2 = InputStreamReader(inputStream2, "UTF-8").readText()
+        pokedex = Gson().fromJson(jsonReader2, PokedexJson::class.java)
+
+    }
+}
